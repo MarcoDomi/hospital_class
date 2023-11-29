7c1db9b96f3bc3a3f4677c3974cbd156eebd414a
Marco Dominguez <marcod082@gmail.com>
2023-11-28 23:56:59 -0600

    finished derived classes for person

--- people.py
+++ people.py
@@ -1,15 +1,13 @@
 class person:
     """represents a human"""
-    def __init__(self, first_name, last_name, age, weight, height) -> None:
+
+    def __init__(self, first_name, last_name, age) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        self.weight = weight
-        self.height = height
 
     def __str__(self) -> str:
-        s = f"Name: {self.first_name} {self.last_name}\n"
-        s += f"Age: {self.age}\nWeight: {self.weight}\nHeight: {self.height}"
+        s = f"Name: {self.first_name} {self.last_name}\nAge: {self.age}"
         return s
 
     def __lt__(self, other: object) -> bool:
@@ -17,16 +15,44 @@ class person:
             return self.last_name < other.last_name
         else:
             return self.first_name < other.first_name
-        
+
     def __gt__(self, other: object) -> bool:
         if self.last_name != other.last_name:
             return self.last_name > other.last_name
         else:
             return self.first_name > other.first_name
         
-p1 = person("mike","larson", 32, 156, "5\'11")
-p2 = person("mike","larson", 43, 178, "6\'7")
-p3 = person("susy","ghett", 22, 126, "5\'01")
-p4 = person("ann","peen", 39, 189, "6\'1")
+class doctor(person):
+    '''represents a doctor'''
+    def __init__(self, first_name, last_name, age, specialty) -> None:
+        super().__init__(first_name, last_name, age)
+        self.specialty = specialty
 
-print(p1)
+    def __str__(self) -> str:
+        s = super().__str__()
+        s += f"\nSpecialty: {self.specialty}"
+        return s
+    
+class nurse(person):
+    '''represents a nurse'''
+    def __init__(self, first_name, last_name, age, type) -> None:
+        super().__init__(first_name, last_name, age)
+        self.type = type #registered or advanced
+
+    def __str__(self) -> str:
+        s = super().__str__()
+        s += f"\nType: {self.type}"
+        return s
+    
+class patient(person):
+    '''represents a patient'''
+    def __init__(self, first_name, last_name, age, height, weight, ailment) -> None:
+        super().__init__(first_name, last_name, age)
+        self.height = height
+        self.weight = weight
+        self.ailment = ailment
+
+    def __str__(self) -> str:
+        s = super().__str__()
+        s += f"\nHeight: {self.height}\nWeight: {self.weight}\nAilment: {self.ailment}"
+        return s
