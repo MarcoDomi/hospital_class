04423bcdf36d8e41fba34ea2ec0acc7b157595d4
Marco Dominguez <marcod082@gmail.com>
2023-11-29 19:02:31 -0600

    added some test code for file handling

--- hospital.py
+++ hospital.py
@@ -14,12 +14,22 @@ class team:
     def __str__(self) -> str:
         s = []
         border = "{:->20}".format("")
-        s += [border, f"Doctor: {self.my_doctor.full_name}","Nurses"]
+        s += [border, f"Doctor: {self.my_doctor.full_name}", "Nurses"]
         for n in self.nurse_group:
             s += [self.nurse_group.full_name]
 
         s += [border]
 
-        return '\n'.join(s)
+        return "\n".join(s)
 
-f = open("txt_files/doctors.txt", "rt")
+
+doc_file = open("txt_files/doctors.txt", "rt")
+
+line = doc_file.readline()
+line_list = line.split(" ")
+
+print(line_list[:2])
+print(" ".join(line_list[:2]))
+print(line_list[2:])
+# for line in doc_file:
+#   print(line)
