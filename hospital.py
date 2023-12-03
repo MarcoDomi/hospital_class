8f4b3977f151fe1dc2ed7eb7787863ab8a5d6fbc
Marco Dominguez <marcod082@gmail.com>
2023-12-02 18:21:49 -0600

    another commit

--- hospital.py
+++ hospital.py
@@ -7,23 +7,23 @@ class team:
 
     def __init__(self, __doctor: doctor, __my_nurses: nurse = None) -> None:
         self.my_doctor = __doctor
-        self.nurse_group = __my_nurses
-        if self.nurse_group == None:
-            self.nurse_group = []
+        self.nurse_team = __my_nurses
+        if self.nurse_team == None:
+            self.nurse_team = []
 
     def __str__(self) -> str:
         s = []
         border = "{:->20}".format("")
         s += [border, f"Doctor: {self.my_doctor.full_name}", "Nurses"]
-        for n in self.nurse_group:
-            s += [self.nurse_group.full_name]
+        for n in self.nurse_team:
+            s += [n.full_name]
 
         s += [border]
 
         return "\n".join(s)
     
-    def add_nurse(self):
-        pass
+    def add_nurse(self, new_nurse):
+        self.nurse_team += [new_nurse]
 
     def pop_nurse(self):
         pass
@@ -32,6 +32,7 @@ class team:
         return self.my_doctor.specialty
     
     
+    #add item retrieval method
     class hospital:
         '''represents a hospital'''
         pass
